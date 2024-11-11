f9f07c7025ed42ac24a77b2d4b6c8d662662f7cc
Gowtham118 <saigowtham@gmail.com>
2024-11-11 23:28:24 +0700

    chore: update react-hooks version

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,10 @@ import App from './App.tsx'
 import './styles/index.css'
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BitcoinNetwork, GardenProvider } from '@gardenfi/react-hooks';
+import { environment, GardenProvider } from '@gardenfi/react-hooks';
 import { wagmiConfig } from './config/wagmi.ts';
-import { API } from './helpers/utils.ts';
 
 const queryClient = new QueryClient();
-const api = API();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -17,12 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <GardenProvider
           config={{
-            orderBookUrl: api.orderbook,
-            quoteUrl: api.quote,
             store: localStorage,
-            network: BitcoinNetwork.Testnet,
-            bitcoinRPCUrl: api.mempool.testnet,
-            blockNumberFetcherUrl: api.data.data,
+            environment: environment.testnet,
           }}
         >
           <App />
